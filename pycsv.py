632242754e3ba032f0d29de7a7ff77f92ce4ba08
Karan0114 <karanch6963@gmail.com>
2026-09-16 10:08:53 +0530

    code added

---
+++ pycsv.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+orders = pd.read_csv("orders.csv", parse_dates = ["order_date"])
+customers = pd.read_csv("customers.csv", parse_dates = ["signup_date"])
+products = pd.read_csv("products.csv")
+
+## (rows, columns)
+#print(f'{orders.shape} \n {customers.shape}\n{products.shape}') 
+
+#print(f'---TOP 5 ORDERS---\n {orders.head(5)}')
+#print(f'---LAST 5 ORDERS---\n {orders.tail(5)}')
+
+#orders.info()
+print(orders[["category", "city"]].describe())
+
